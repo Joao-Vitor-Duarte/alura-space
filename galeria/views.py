f6d0f179538ab19f2636f0f036e672e352b59e8d
João Vitor T S Duarte <joao_vitor_0@hotmail.com>
2024-04-16 17:43:41 -0300

    criação do app usuarios

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render,get_object_or_404
 from galeria.models import Fotografia
 def index(request):
-    fotos = Fotografia.objects.all()
+    fotos = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request,'galeria/index.html',{"cards": fotos})
 def imagem(request,foto_id):
     fotos=get_object_or_404(Fotografia,pk=foto_id)
     return render(request,'galeria/imagem.html',{'fotografia':fotos}) 
+def buscar(request):
+    fotos=Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        busca = request.GET['buscar'] 
+        if busca:
+            fotos=fotos.filter(nome__icontains=busca)
+
+    return render(request,'galeria/buscar.html',{'cards':fotos}) 
