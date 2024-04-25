442ebf7e01f1d80d3bea905cc1ad0030ad2c9755
João Vitor T S Duarte <joao_vitor_0@hotmail.com>
2024-04-25 12:42:01 -0300

    melhoria de autenticação de usuários

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from galeria.models import Fotografia
+from django.contrib import messages
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request,'usuário não logado')
+        return redirect('login')
     fotos = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request,'galeria/index.html',{"cards": fotos})
 def imagem(request,foto_id):
+    
     fotos=get_object_or_404(Fotografia,pk=foto_id)
     return render(request,'galeria/imagem.html',{'fotografia':fotos}) 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request,'usuário não logado')
+        return redirect('login')
+    
     fotos=Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     
     if 'buscar' in request.GET:
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from usuarios.views import login,cadastro,logout
 
-urlpatters =[
-
+urlpatterns =[
+    path('login', login, name='login'),
+    path('logout',logout,name='logout'),
+    path('cadastro', cadastro , name='cadastro'),
 ]
